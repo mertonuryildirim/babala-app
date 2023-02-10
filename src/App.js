7f77a3a960ecf49add18d79e9d99eda76984cac4
Mert Onur Yıldırım <mertonuryildirim@gmail.com>
2023-02-10 18:17:11 +0300

    added required libraries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
-    <div className="App">
-      Babala App
-    </div>
+    <Stack direction="row" spacing={2}>
+    <Button>Primary</Button>
+    <Button disabled>Disabled</Button>
+    <Button href="#text-buttons">Link</Button>
+  </Stack>
   );
 }
 
