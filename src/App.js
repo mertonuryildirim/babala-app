41c40b635d7efc00adca88be249e26abd1315794
Mert Onur Yıldırım <mertonuryildirim@gmail.com>
2023-02-11 02:44:35 +0300

    folder structure changed

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Dashboard from './Pages/Dashboard';
-import List from './Pages/List';
+import Error from './Pages/Error';
 
 function App() {
     return (
         <div style={{ background: '#f0f0f0', height: 'auto' }}>
             <Routes>
                 <Route path="/" element={<Dashboard />} />
-                <Route path="list" element={<List />} />
-                <Route
-                    path="*"
-                    element={
-                        <div style={{ padding: '38% 44%' }}>
-                            404 Page Not Found
-                        </div>
-                    }
-                />
+                <Route path="*" element={<Error />} />
             </Routes>
         </div>
     );
---
+++ src/Pages/Error.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Error() {
+    return <div style={{ padding: '38% 44%' }}>404 Page Not Found</div>;
+}
+
+export default Error;
