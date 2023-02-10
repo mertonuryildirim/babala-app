c838504d937dc0ebe003f595bef7f0d06f52e866
ahmet-furtun <ahmet.furtun1@gmail.com>
2023-02-10 19:34:11 +0300

    layout css added, summary started

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+const container = {
+    width: '100vw',
+    display: 'flex',
+    justifyContent: 'space-around',
+};
+
+const boxContainer = {
+    width: 500,
+    height: 250,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ddd',
+};
+
 const Summary = () => {
-    return <div>Summary</div>;
+    return (
+        <div style={container}>
+            <div style={boxContainer}>1</div>
+            <div style={boxContainer}>2</div>
+        </div>
+    );
 };
 
 export default Summary;
